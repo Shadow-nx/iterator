fdbcb9f1c366a86e97831712bd224359918b1593
Shadow-nx <32244138+Shadow-nx@users.noreply.github.com>
2018-05-18 13:39:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,15 +8,9 @@ int main() {
     Tree<int> tree = {5, 3, 4, 2, 7, 9, 6, 8};
 
     auto it = tree.begin();
-    auto end = tree.end();
+    //auto end = tree.end();
 
-    for (; it != end; ++it ) {
-          std::cout << *it << ' ';
-      }
-
-    std::cout<<" lol" << std::endl;
-
-    for (; it != end; ++it ) {
+    for (int i=0; i < 8; ++it,i++ ) {
           std::cout << *it << ' ';
       }
 

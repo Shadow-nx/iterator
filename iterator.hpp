b0520c3ce5493490d1a271bf4a4abfe8e343ee72
Shadow-nx <32244138+Shadow-nx@users.noreply.github.com>
2018-06-13 18:19:31 +0300

    Update iterator.hpp

--- iterator.hpp
+++ iterator.hpp
@@ -20,167 +20,167 @@ namespace BSTree {
         using value_type = T;
 
     public:
-    /*
-        iterator();
+        iterator(){
+            ptr=nullptr;
+        }
 
-        iterator(const iterator &);
-
-        ~iterator();
-
-        iterator& operator=(const iterator &);
-
-
-
-        virtual iterator& operator++()=0;
-
-        virtual reference operator*()=0;
-        virtual iterator operator++(int);
-
-        value_type operator*() const;
-
-        pointer operator->() const;
-
-        reference operator*() const;
-
-        iterator operator++(int);
-        template <typename T2>
-        friend bool operator==(const iterator&, const iterator&);
-        template <typename T3>
-        friend bool operator!=(const iterator&, const iterator&);
-        template <typename T4>
-        friend void swap(iterator&, iterator&);
-*/
     private:
         Node<T>* ptr;
     };
+    
 
     template <typename T>
     class pre_iterator : iterator<T> {
     public:
-        Road<T>* node;
         pre_iterator();
-        pre_iterator(Node<T>* ptr);
+        pre_iterator(Node<T>* ptr,Node<T>* troot,T data);
         auto operator *() -> T&;
+        auto operator--() -> pre_iterator<T>&;
         auto operator++() ->pre_iterator<T>&;
-        auto direct_bypass(Node<T>* root) -> void;
-        auto insert(Node<T>* root) -> void;
+        auto simetryc() -> bool;
         template <typename T2>
-        auto friend operator!=( pre_iterator<T2>&, pre_iterator<T2>&) -> bool;
-    // ...
+        friend auto operator!=(pre_iterator<T2>& lhs, pre_iterator<T2>& rhs) -> bool{
+            if(lhs.node!=rhs.node)
+                return true;
+            else
+                return false;
+        }
+    private:
+        Node<T>* root;
+        Node<T>* node;
+        T value;
     };
 
     template <typename T>
     class post_iterator : iterator<T> {
-    private:
-        Road<T>* node;
-    protected:
+    public:
         post_iterator();
         post_iterator(Node<T>* ptr);
-        /*
-        auto operator *() -> T&;
-        auto operator++() ->post_iterator<T>&;
-        auto direct_bypass(Node<T>* root) -> void;
-        auto insert(Node<T>* root) -> void;
-         template <typename T2>
-         */
-    // ...
+    private:
+        Node<T>* node;
     };
-
+    
+    
     template <typename T>
     pre_iterator<T>::pre_iterator(){
         node=nullptr;
+        root=nullptr;
+        value=0;
     }
 
     template <typename T>
-    pre_iterator<T>::pre_iterator(Node<T>* ptr){
+    pre_iterator<T>::pre_iterator(Node<T>* ptr,Node<T>* troot,T data){
         node=nullptr;
-        direct_bypass(ptr);
+        root=nullptr;
+        node=ptr;
+        root=node;
+        value=data;
     }
-
+    
     template <typename T>
-    auto pre_iterator<T>::direct_bypass(Node<T>* root) -> void {
-        if(root!=nullptr){
-            insert(root);
-    		direct_bypass(root->right);
-    		direct_bypass(root->left);
-		}
+    auto simetryc() -> bool{
+        
     }
-
-    template <typename T>
-    auto pre_iterator<T>::insert(Node<T>* root) -> void {
-        if(node==nullptr){
-            node=new Road<T>{root,nullptr};
-            return;
-        }
-        Road<T>* current=node;
-        while(current->next!=nullptr && current->next!=node){
-           current=current->next;
-        }
-        current->next=new Road<T>{root,node};
-    }
-
+    
     template <typename T>
     auto pre_iterator<T>::operator *() -> T&{
-       return node->curr->data;
+        if(node!=nullptr){
+            if(node->test==false)
+                node->test=true;
+            else if(node->test==true)
+                node->test=false;
+            if(node->data!=nullptr)
+                return *(node->data);
+        }
     }
-
+    template <typename T>
+    auto pre_iterator<T>::operator--() -> pre_iterator<T>&{
+            while(true){
+                if(node->parent==nullptr){
+                    if(node->left!=nullptr && node->left->test==true){
+                        node=node->left;
+                        if(node->right!=nullptr && node->right->test==true)
+                            continue;
+                        else
+                            break;
+                    }
+                }else if(node==node->parent->right){
+                    if(node->left!=nullptr && node->left->test==true){
+                        node=node->left;
+                        if(node->right!=nullptr && node->right->test==true)
+                            continue;
+                        else
+                            break;
+                    }else if((node->left==nullptr || (node->left!=nullptr && node->left->test==false)) && node->parent!=nullptr){
+                        if(node->parent!=nullptr)
+                            node=node->parent;
+                        break;
+                    }
+                }else if(node==node->parent->left){
+                    if(node->right!=nullptr && node->right->test==true){
+                        while(node->right!=nullptr && node->right->test==true){
+                            node=node->right;
+                        }
+                        break;
+                    }else if((node->right==nullptr || node->right!=nullptr) && node->left!=nullptr && node->left->test==true){
+                        node=node->left;
+                        if(node->right!=nullptr)
+                            continue;
+                        else
+                            break;
+                    }else{
+                        if(node->parent!=nullptr)
+                            node=node->parent;
+                        continue;
+                    }
+                    break;
+                }
+            }
+                return *this; 
+    }
     template <typename T>
     auto pre_iterator<T>::operator++() ->pre_iterator<T>&{
-        node=node->next;
-        return *this;
-    }
-
-    template <typename T2>
-     auto operator!=(pre_iterator<T2>& l, pre_iterator<T2>& r) -> bool
-    {
-            if(l.node->curr!=r.node->curr)
-            return true;
-
-                return false;
-    }
-/*
-    template <typename T>
-    post_iterator<T>::post_iterator(){
-        node=nullptr;
-    }
-
-    template <typename T>
-    post_iterator<T>::post_iterator(Node<T>* ptr){
-        node=nullptr;
-        direct_bypass(ptr);
-    }
-
-    template <typename T>
-    auto post_iterator<T>::direct_bypass(Node<T>* root) -> void {
-        if(root!=nullptr){
-            insert(root);
-    		direct_bypass(root->right);
-    		direct_bypass(root->left);
-		}
-    }
-
-    template <typename T>
-    auto post_iterator<T>::insert(Node<T>* root) -> void {
-        if(node==nullptr){
-            node=new Road<T>{root,nullptr};
-            return;
+        while(true){
+            if(node->parent==nullptr){
+                if(node->right!=nullptr && node->right->test==false){
+                    node=node->right;
+                    continue;
+                }else if(node->right==nullptr){
+                    break;
+                }
+            }else if(node==node->parent->left){
+                if(node->right!=nullptr && node->right->test==false)
+                {
+                    node=node->right;
+                    if(node->left!=nullptr)
+                        continue;
+                    else
+                        break;
+                }else if((node->right==nullptr || (node->right!=nullptr && node->right->test==true))&&node->parent!=nullptr){
+                    if(node->parent!=nullptr)
+                        node=node->parent;
+                    break;
+                }
+                break;
+            }else if(node==node->parent->right){
+                if(node->left!=nullptr && node->left->test==false){
+                    while(node->left!=nullptr && node->left->test==false){
+                        node=node->left;
+                    }
+                    break;
+                }else if((node->left==nullptr || node->left!=nullptr) && node->right!=nullptr && node->right->test==false){
+                    node=node->right;
+                    if(node->left!=nullptr)
+                        continue;
+                    else
+                        break;
+                }else{
+                    if(node->parent!=nullptr)
+                        node=node->parent;
+                    continue;
+                }
+            }
         }
-        Road<T>* current=node;
-        while(current->next!=nullptr && current->next!=node){
-           current=current->next;
-        }
-        current->next=new Road<T>{root,node};
-    }
-
-    template <typename T>
-    auto post_iterator<T>::operator *() -> T&{
-       return node->curr->data;
-    }
-
-    template <typename T>
-    auto post_iterator<T>::operator++() ->post_iterator<T>&{
-        node=node->next;
-        return *this;
-    }
-    */
+        return *this; 
+    }  
 }
